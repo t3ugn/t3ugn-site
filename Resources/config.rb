9b8cbdace61a721bfcafc25808d61747c673347a
Sascha Nowak <sascha.nowak@netlogix.de>
2014-07-28 22:09:37 +0200

    [!!!][FEATURE| Add default layout and add some content elements

---
+++ Resources/config.rb
@@ -0,0 +1,24 @@
+#Include Bootstrap with full path for PhpStorm autocomplete and compass support
+add_import_path "bower_components/foundation/scss"
+
+# Sass Paths
+http_path = "/_Resources/Static/Packages/T3UGN.T3ugnDe/"
+
+# Sass Directories
+css_dir = "Public/Styles"
+sass_dir = "Private/Scss"
+images_dir = "Public/Assets/Images"
+javascripts_dir = "Public/Script"
+fonts_dir = "Public/Assets/Fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+sourcemap = true
